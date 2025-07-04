1d5e56b787c04c9ededfe8fcb7849aa6c7b2c424
loicmaes <97891718+loicmaes@users.noreply.github.com>
2025-07-05 00:12:07 +0200

    :rocket: setup default layouts

---
+++ layouts/authentication.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div
+    data-layout="authentication"
+    class="min-h-dvh grid place-items-center p-6"
+  >
+    <NuxtPage />
+  </div>
+</template>
